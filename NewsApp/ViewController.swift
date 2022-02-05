0a74ed73a8ae17cb979cb9b54236079444728985
Muhannad BakBouk <MuhannadBakBouk@gmail.com>
2022-02-05 05:24:36 -0800

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Mohannad on 2/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
